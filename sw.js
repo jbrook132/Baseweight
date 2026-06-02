a2e2cd4b1938859beddafd890ee58131b0ffb90e
jbrook132 <jack.brookes@uqconnect.edu.au>
2026-06-02 10:23:37 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "baseweight-v15";
+const CACHE = "baseweight-v16";
 
 // self.registration.scope resolves to the actual deployment URL,
 // so this works whether hosted at root or a subdirectory (e.g. /baseweight/)
